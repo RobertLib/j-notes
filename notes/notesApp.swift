742fd57ca35da6c02b0e59f7d2d010a7d865a63e
RobertLib <rob@hotmail.cz>
2022-07-05 19:07:02 +0200

    Initial Commit

---
+++ notes/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes
+//
+//  Created by Robert Libšanský on 05.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
